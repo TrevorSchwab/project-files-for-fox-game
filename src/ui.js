689cd58533f43abd63c063b367c1ec42d80cfd1a
Trevor Schwab <trevschwab@gmail.com>
2020-10-08 21:13:32 -0500

    finished game

---
+++ src/ui.js
@@ -0,0 +1,19 @@
+import { doc } from "prettier";
+
+export const modFox = (state) => {
+	document.querySelector(".fox").className = `fox fox-${state}`;
+};
+
+export const modScene = (state) => {
+	document.querySelector(".game").className = `game ${state}`;
+};
+
+export const togglePoopBag = (show) => {
+	document.querySelector(".poop-bag").classList.toggle("hidden", !show);
+};
+
+export const writeModal = (text = "") => {
+	document.querySelector(
+		".modal"
+	).innerHTML = `<div class="modal-inner">${text}</div>`;
+};
